33f62966f44b00616c826d84d825c692057d00f7
michellviu <michellviu@gmail.com>
2026-02-19 01:22:09 -0500

    feat: Add migrations

---
+++ apps/api/src/Infrastructure.DermaImage/Migrations/20260219060807_AddRequiredRelationsForImages.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.DermaImage.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRequiredRelationsForImages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Delete images that have no institution or contributor assigned,
+            // because those columns are being made NOT NULL below.
+            migrationBuilder.Sql(
+                """
+                DELETE FROM "Images"
+                WHERE "InstitutionId" IS NULL OR "ContributorId" IS NULL;
+                """);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PublicId",
+                table: "Images",
+                type: "character varying(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "InstitutionId",
+                table: "Images",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ContributorId",
+                table: "Images",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("a1b2c3d4-0001-0000-0000-000000000001"),
+                column: "ConcurrencyStamp",
+                value: "d3c8b1ea-f6a1-473b-ad63-b536b4cffcfc");
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("a1b2c3d4-0002-0000-0000-000000000002"),
+                column: "ConcurrencyStamp",
+                value: "01e3e9e5-e364-44d9-9c43-49699c32cc19");
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("a1b2c3d4-0003-0000-0000-000000000003"),
+                column: "ConcurrencyStamp",
+                value: "fb6c7b75-f57a-4665-9ada-f152f9907452");
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("a1b2c3d4-0004-0000-0000-000000000004"),
+                column: "ConcurrencyStamp",
+                value: "27804fd8-0e9d-4ea2-af30-e638e994c174");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PublicId",
+                table: "Images",
+                type: "character varying(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(30)",
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "InstitutionId",
+                table: "Images",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ContributorId",
+                table: "Images",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("a1b2c3d4-0001-0000-0000-000000000001"),
+                column: "ConcurrencyStamp",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("a1b2c3d4-0002-0000-0000-000000000002"),
+                column: "ConcurrencyStamp",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("a1b2c3d4-0003-0000-0000-000000000003"),
+                column: "ConcurrencyStamp",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("a1b2c3d4-0004-0000-0000-000000000004"),
+                column: "ConcurrencyStamp",
+                value: null);
+        }
+    }
+}
