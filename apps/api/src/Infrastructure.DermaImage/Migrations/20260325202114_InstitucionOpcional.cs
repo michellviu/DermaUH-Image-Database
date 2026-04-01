603a6812be69378883131ca4684185398aa4c3bb
michellviu <michellviu@gmail.com>
2026-04-01 17:00:48 -0400

    db(migrations): make institution optional for images
    - Add migration for optional Institution relationship
    - Update DermaImg entity to allow null Institution
    - Maintain backward compatibility with existing data

---
+++ apps/api/src/Infrastructure.DermaImage/Migrations/20260325202114_InstitucionOpcional.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.DermaImage.Migrations
+{
+    /// <inheritdoc />
+    public partial class InstitucionOpcional : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "InstitutionId",
+                table: "Images",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "InstitutionId",
+                table: "Images",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+        }
+    }
+}
