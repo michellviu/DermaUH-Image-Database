6407da38b6f11c9a6d549c1f2c928d46c8f1f480
michellviu <michellviu@gmail.com>
2026-04-03 16:42:21 -0400

    feat(domain): add filter support for InjuryType and PhotoType
    - Add InjuryTypes and FotoTypes filter collections to DermaImgFilter
    - Enable filtering images by lesion type and skin type

--- apps/api/src/Domain.DermaImage/Entities/DermaImgFilter.cs
+++ apps/api/src/Domain.DermaImage/Entities/DermaImgFilter.cs
@@ -6,6 +6,8 @@ public class DermaImgFilter
 {
     public IReadOnlyCollection<ImageType>? ImageTypes { get; set; }
     public IReadOnlyCollection<DiagnosisCategory>? DiagnosisCategories { get; set; }
+    public IReadOnlyCollection<InjuryType>? InjuryTypes { get; set; }
+    public IReadOnlyCollection<PhotoType>? FotoTypes { get; set; }
     public IReadOnlyCollection<Sex>? Sexes { get; set; }
     public IReadOnlyCollection<AnatomSiteGeneral>? AnatomSites { get; set; }
     public bool? IsPublic { get; set; }
