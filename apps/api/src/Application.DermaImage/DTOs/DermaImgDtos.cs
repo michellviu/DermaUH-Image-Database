85d9abfc92f85aee16e79542b1589c96773d97cf
michellviu <michellviu@gmail.com>
2026-05-14 12:40:36 -0400

    feat(api): remove image upload review workflow DTOs
    - Remove ReviewImageUploadDto class
    - Remove review-related fields from DermaImgResponseDto:
      ApprovalStatus, ReviewComment, ReviewedAt, ReviewedByUserId, ReviewedByFullName
    - Remove unused DataAnnotations import

--- apps/api/src/Application.DermaImage/DTOs/DermaImgDtos.cs
+++ apps/api/src/Application.DermaImage/DTOs/DermaImgDtos.cs
@@ -1,5 +1,4 @@
 using Domain.DermaImage.Entities.Enums;
-using System.ComponentModel.DataAnnotations;
 
 namespace Application.DermaImage.DTOs;
 
@@ -57,11 +56,6 @@ public class DermaImgResponseDto
     public string ContentType { get; set; } = string.Empty;
     public long FileSize { get; set; }
     public bool IsPublic { get; set; }
-    public ImageApprovalStatus ApprovalStatus { get; set; }
-    public string? ReviewComment { get; set; }
-    public DateTime? ReviewedAt { get; set; }
-    public Guid? ReviewedByUserId { get; set; }
-    public string? ReviewedByFullName { get; set; }
 
     // Acquisition
     public ImageType? ImageType { get; set; }
@@ -99,14 +93,6 @@ public class DermaImgResponseDto
     public string? ContributorFullName { get; set; }
     public Guid? InstitutionId { get; set; }
     public string? InstitutionName { get; set; }
-}
-
-public class ReviewImageUploadDto
-{
-    public bool Approve { get; set; }
-
-    [MaxLength(500)]
-    public string? Comment { get; set; }
 }
 
 public class DownloadImagesRequest
