8cd9ed394b59bff7c53a2bb2c41d34312bfe0384
michellviu <michellviu@gmail.com>
2026-05-25 19:01:35 -0400

    feat(migrations): add Contributor role to Roles table and update model snapshot

---
+++ apps/api/src/Infrastructure.DermaImage/Migrations/20260525225754_AddRole.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.DermaImage.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { new Guid("a1b2c3d4-0002-0000-0000-000000000002"), "caa0f0fa-b5e0-4d66-9f6f-f15e69c9f15b", "Contributor", "CONTRIBUTOR" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("a1b2c3d4-0002-0000-0000-000000000002"));
+        }
+    }
+}
